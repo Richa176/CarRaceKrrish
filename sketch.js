32d510b546f64ae0de2523a626f6a89eb69f57b7
Richa176 <69035220+Richa176@users.noreply.github.com>
2021-05-06 18:39:17 +0530

    edited

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/trk2.png");
-  car1_img = loadImage("../images/Bcar.png");
-  car2_img = loadImage("../images/Rcar.png");
-  car3_img = loadImage("../images/Ycar.png");
+  track = loadImage("trk2.png");
+  car1_img = loadImage("Bcar.png");
+  car2_img = loadImage("Rcar.png");
+  car3_img = loadImage("Ycar.png");
   //car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  ground = loadImage("ground.png");
 }
 
 function setup(){
